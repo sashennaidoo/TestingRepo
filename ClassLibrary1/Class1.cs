1426cbeb83e5a454e0e6d400138791a202e1d114
sashen <naidoo.sashen@gmail.com>
2016-11-08 11:01:08 +0200

    Test

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var t = 1;
+            Debug.Write(t);
+            // do something
+        }
+    }
+}
